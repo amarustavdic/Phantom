bd6537a99eaafca5361b28fe9df477e0d7a69353
amar <ustavdica.private@gmail.com>
2024-12-28 04:12:17 +0100

    Created representation of the board using bitboards, and print funciton for the state

---
+++ src/main/java/com/ustavdica/State.java
@@ -0,0 +1,81 @@
+package com.ustavdica;
+
+/**
+ *  Represents the state of the game called 4mation.
+ */
+public class State {
+
+    private long[] bitboards;
+    private long metadata;
+
+    public State(Player startingPlayer) {
+        this.bitboards = new long[2];
+        this.metadata = 0L;
+
+        // Set the starting player in the metadata
+        if (startingPlayer == Player.PINK) {
+            metadata |= 0x8000000000000000L; // Set MSB for PINK
+        }
+    }
+
+
+
+
+    /**
+     *  Prints the current state of the game board to the console.<br>
+     *  <br>
+     *  The board is displayed as a 7x7 grid. Each cell shows the square number<br>
+     *  and is color-coded based on the player occupying the square:<br>
+     *  <br>
+     *  - Blue squares are highlighted with a blue background.<br>
+     *  - Pink squares are highlighted with a pink background.<br>
+     *  - Empty squares are uncolored.<br>
+     *  <br>
+     *  Square numbering starts from 0 (bottom-right corner) and increments<br>
+     *  right-to-left, bottom-to-top.<br>
+     *  <br>
+     *  Example Output:<br>
+     *  ----------------<br>
+     *  48 47 46 45 44 43 42<br>
+     *  41 40 39 38 37 36 35<br>
+     *  ...<br>
+     *  6  5  4  3  2  1  0<br>
+     *  <br>
+     *  Color Key:<br>
+     *  - Blue: Player.BLUE<br>
+     *  - Pink: Player.PINK<br>
+     */
+    public void print() {
+        long blueBoard = bitboards[Player.BLUE.ordinal()];
+        long pinkBoard = bitboards[Player.PINK.ordinal()];
+
+        blueBoard = blueBoard << 15;
+        pinkBoard = pinkBoard << 15;
+
+        StringBuilder sb = new StringBuilder();
+        for (int row = 7; row > 0; row--) {
+
+            for (int col = 0; col < 7 ; col++) {
+                int square = (row * 7) - col - 1;
+
+                long bit = 1L << square << 15;
+
+                String background = "\u001B[0m";
+                if ((blueBoard & bit) > 0) {
+                    background = "\u001B[48;5;21m";
+                }
+
+                if ((pinkBoard & bit) > 0) {
+                    background = "\u001B[48;5;201m";
+                }
+
+                sb.append(background);
+                if (square < 10) sb.append(" ");
+                sb.append(square).append(' ').append("\u001B[0m");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
