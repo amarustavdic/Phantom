6aef59e9cd9872ac04b3d7f4629cba3e6d55ac45
amar <ustavdica.private@gmail.com>
2024-12-29 02:59:13 +0100

    Comment update

--- src/main/java/com/ustavdica/state/StateHandler.java
+++ src/main/java/com/ustavdica/state/StateHandler.java
@@ -14,6 +14,7 @@ public class StateHandler {
 
     private static StateHandler instance;
 
+    // Precomputed masks for speed
     private final long[] squareMasks;
     private final long[] squareOutlineMasks;
 
